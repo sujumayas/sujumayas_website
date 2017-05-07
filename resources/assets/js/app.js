84de047501461ccfc66c2681a88c8c7a27af08f2
Esen <sujumayas@gmail.com>
2017-05-07 00:54:33 -0500

    keenui installed

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -6,9 +6,9 @@
  */
 
 require('./bootstrap');
+// require('./vendor/src/index.js');
 
 window.Vue = require('vue');
-
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
@@ -16,7 +16,55 @@ window.Vue = require('vue');
  */
 
 Vue.component('example', require('./components/Example.vue'));
+Vue.component('ui-alert', require('./vendor/src/UiAlert.vue'));
+Vue.component('ui-autocomplete', require('./vendor/src/UiAutocomplete.vue'));
+Vue.component('ui-button', require('./vendor/src/UiButton.vue'));
+Vue.component('ui-calendar', require('./vendor/src/UiCalendar.vue'));
+Vue.component('ui-checkbox', require('./vendor/src/UiCheckbox.vue'));
+Vue.component('ui-checkbox-group', require('./vendor/src/UiCheckboxGroup.vue'));
+Vue.component('ui-collapsible', require('./vendor/src/UiCollapsible.vue'));
+Vue.component('ui-confirm', require('./vendor/src/UiConfirm.vue'));
+Vue.component('ui-datepicker', require('./vendor/src/UiDatepicker.vue'));
+Vue.component('ui-fab', require('./vendor/src/UiFab.vue'));
+Vue.component('ui-fileupload', require('./vendor/src/UiFileupload.vue'));
+Vue.component('ui-icon', require('./vendor/src/UiIcon.vue'));
+Vue.component('ui-icon-button', require('./vendor/src/UiIconButton.vue'));
+Vue.component('ui-menu', require('./vendor/src/UiMenu.vue'));
+Vue.component('ui-modal', require('./vendor/src/UiModal.vue'));
+Vue.component('ui-popover', require('./vendor/src/UiPopover.vue'));
+Vue.component('ui-preloader', require('./vendor/src/UiPreloader.vue'));
+Vue.component('ui-progress-circular', require('./vendor/src/UiProgressCircular.vue'));
+Vue.component('ui-progress-linear', require('./vendor/src/UiProgressLinear.vue'));
+Vue.component('ui-radio', require('./vendor/src/UiRadio.vue'));
+Vue.component('ui-radio-group', require('./vendor/src/UiRadioGroup.vue'));
+Vue.component('ui-ripple-ink', require('./vendor/src/UiRippleInk.vue'));
+Vue.component('ui-select', require('./vendor/src/UiSelect.vue'));
+Vue.component('ui-slider', require('./vendor/src/UiSlider.vue'));
+Vue.component('ui-snackbar', require('./vendor/src/UiSnackbar.vue'));
+Vue.component('ui-snackbar-container', require('./vendor/src/UiSnackbarContainer.vue'));
+Vue.component('ui-switch', require('./vendor/src/UiSwitch.vue'));
+Vue.component('ui-tab', require('./vendor/src/UiTab.vue'));
+Vue.component('ui-tabs', require('./vendor/src/UiTabs.vue'));
+Vue.component('ui-textbox', require('./vendor/src/UiTextbox.vue'));
+Vue.component('ui-toolbar', require('./vendor/src/UiToolbar.vue'));
+Vue.component('ui-tooltip', require('./vendor/src/UiTooltip.vue'));
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    data: {
+    	showAlert1: false,
+    	file11PreviewImage: '',
+    },
+    methods: {
+    	openModal(ref) {
+            this.$refs[ref].open();
+        },
+        closeModal(ref) {
+            this.$refs[ref].close();
+        },
+        onFile11Change(files) {
+            this.file11PreviewImage = URL.createObjectURL(files[0]);
+        }
+    }
 });
+
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,173 @@
 <!doctype html>
 <html lang="{{ config('app.locale') }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Sujumayas</title>
+    <title>Sujumayas</title>
+    
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    
+    <!-- Styles -->
+    <style>
+      html, body {
+        background-color: #fff;
+        color: #636b6f;
+        font-family: 'Raleway', sans-serif;
+        font-weight: 100;
+        height: 100vh;
+        margin: 0;
+      }
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+      .full-height {
+        height: 100vh;
+      }
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+      .flex-center {
+        align-items: center;
+        display: flex;
+        justify-content: center;
+      }
 
-            .full-height {
-                height: 100vh;
-            }
+      .position-ref {
+        position: relative;
+      }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+      .top-right {
+        position: absolute;
+        right: 10px;
+        top: 18px;
+      }
 
-            .position-ref {
-                position: relative;
-            }
+      .content {
+        text-align: center;
+      }
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+      .title {
+        font-size: 84px;
+      }
 
-            .content {
-                text-align: center;
-            }
+      .links > a {
+        color: #636b6f;
+        padding: 0 25px;
+        font-size: 12px;
+        font-weight: 600;
+        letter-spacing: .1rem;
+        text-decoration: none;
+        text-transform: uppercase;
+      }
 
-            .title {
-                font-size: 84px;
-            }
+      .m-b-md {
+        margin-bottom: 30px;
+      }
+      .page__demo-preview {
+        display: block;
+        position: absolute;
+        width:100%;
+        top: 0;
+        left:0;
+        z-index:1;
+      }
+      .page__demo-preview img {
+        width:100%;
+        filter: blur(10px);
+      }
+      h1{
+        z-index:9999;
+      }
+      .title,
+      .alerts{
+        background-color:white;
+      }
+      .allContentHereToFixZindex{
+        position: relative;
+        z-index:999;
+      }
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Sujumayas
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+    </style>
+  </head>
+  <body>
+    <div class="flex-center position-ref full-height">
+      @if (Route::has('login'))
+        <div class="top-right links">
+          @if (Auth::check())
+            <a href="{{ url('/home') }}">Home</a>
+          @else
+            <a href="{{ url('/login') }}">Login</a>
+            <a href="{{ url('/register') }}">Register</a>
+          @endif
         </div>
-    </body>
+      @endif
+
+      <div class="content" id="app">
+        {{-- Background Image --}}
+        <div class="page__demo-preview" v-if="file11PreviewImage.length > 0">
+          <img alt="file11PreviewName" class="page__demo-preview-image" :src="file11PreviewImage">
+        </div>
+        {{-- End of Background Image --}}
+        <div class="allContentHereToFixZindex">
+          {{-- Title --}}
+          <div class="title m-b-md">
+            Sujumayas
+          </div>
+          {{-- End of Title --}}
+          {{-- <example></example> --}}
+          <div id="alerts">
+            <ui-alert @dismiss="showAlert1 = false" type="error" v-show="showAlert1">
+              Oh My God! Becarefull with this stuff!
+            </ui-alert>
+          </div>
+          {{-- Main Content --}}
+          <hr>
+          <div>
+            {{-- <ui-autocomplete></ui-autocomplete> --}}
+            <ui-button ref="sayhi" @click="openModal('myModal')">Say Hi</ui-button>
+            <ui-button ref="warning" @click="showAlert1=!showAlert1">Activate Warning!</ui-button>
+            {{-- <ui-calendar></ui-calendar> --}}
+            {{-- <ui-checkbox></ui-checkbox> --}}
+            {{-- <ui-checkbox-group></ui-checkbox-group> --}}
+            {{-- <ui-collapsible></ui-collapsible> --}}
+            {{-- <ui-confirm></ui-confirm> --}}
+            {{-- <ui-datepicker></ui-datepicker> --}}
+            {{-- <ui-fab></ui-fab> --}}
+            {{-- <ui-fileupload></ui-fileupload> --}}
+            {{-- <ui-icon></ui-icon> --}}
+            {{-- <ui-icon-button></ui-icon-button> --}}
+            {{-- <ui-menu></ui-menu> --}}
+            {{-- <ui-popover></ui-popover> --}}
+            {{-- <ui-preloader></ui-preloader> --}}
+            {{-- <ui-progress-circular></ui-progress-circular> --}}
+            {{-- <ui-progress-linear></ui-progress-linear> --}}
+            {{-- <ui-radio></ui-radio> --}}
+            {{-- <ui-radio-group></ui-radio-group> --}}
+            {{-- <ui-ripple-ink></ui-ripple-ink> --}}
+            {{-- <ui-select></ui-select> --}}
+            {{-- <ui-slider name="Slider" :value="1"></ui-slider> --}}
+            {{-- <ui-snackbar></ui-snackbar> --}}
+            {{-- <ui-snackbar-container></ui-snackbar-container> --}}
+            {{-- <ui-switch></ui-switch> --}}
+            {{-- <ui-tab></ui-tab> --}}
+            {{-- <ui-tabs></ui-tabs> --}}
+            {{-- <ui-textbox></ui-textbox> --}}
+            {{-- <ui-toolbar></ui-toolbar> --}}
+            {{-- <ui-tooltip></ui-tooltip> --}}
+          </div>
+          {{-- File Upload --}}
+          <div>
+            <br>
+            <br>
+            <ui-fileupload accept="image/*" name="file11" @change="onFile11Change">
+              Upload a momentary Background Image
+            </ui-fileupload>
+          </div>
+          <hr>
+          <ui-modal ref="myModal" title="Greeting Modal">Hello there!</ui-modal>
+        </div>
+      </div>
+    </div>
+    <!-- critical JS -->
+    <script src="/js/app.js"></script>
+  </body>
 </html>
